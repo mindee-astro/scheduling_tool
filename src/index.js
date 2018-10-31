22a6a17c9b7636c558cdb96f3bb2d0f950606044
ahmadakmaluddin <ahmad-akmaluddin_ahmad@astro.com.my>
2018-10-31 10:09:06 +0800

    Added standard theme template.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 const rootEl = document.getElementById('root');
+const theme = createMuiTheme(
+	{
+		palette: {
+	    	type: 'light', // Switching the dark mode on is a single property value change.
+	  	},
+	  	typography: {
+		  	fontFamily: [
+		      '-apple-system',
+		      'BlinkMacSystemFont',
+		      '"Segoe UI"',
+		      'Roboto',
+		      '"Helvetica Neue"',
+		      'Arial',
+		      'sans-serif',
+		      '"Apple Color Emoji"',
+		      '"Segoe UI Emoji"',
+		      '"Segoe UI Symbol"',
+		    ].join(','),
+		    fontSize: 15,
+		},
+		overrides: {
+		    MuiButton: {
+		      	root: {
+		      		fontFamily: [
+				      '-apple-system',
+				      'BlinkMacSystemFont',
+				      '"Segoe UI"',
+				      'Roboto',
+				      '"Helvetica Neue"',
+				      'Arial',
+				      'sans-serif',
+				      '"Apple Color Emoji"',
+				      '"Segoe UI Emoji"',
+				      '"Segoe UI Symbol"',
+				    ].join(','),
+			        borderRadius: 15,
+			        border: 1,
+			        color: 'black',
+			        padding: '0 30px',
+		      	},
+		    },
+		    MuiCard: {
+		      	root: {
+			        borderRadius: 5,
+			        border: 0,
+			        color: 'black',
+			        padding: '0 30px',
+			        boxShadow: '0 3px 5px 2px rgba(100, 100, 100, .3)',
+			        fontFamily: [
+				      '-apple-system',
+				      'BlinkMacSystemFont',
+				      '"Segoe UI"',
+				      'Roboto',
+				      '"Helvetica Neue"',
+				      'Arial',
+				      'sans-serif',
+				      '"Apple Color Emoji"',
+				      '"Segoe UI Emoji"',
+				      '"Segoe UI Symbol"',
+				    ].join(','),
+		      	},
+		    },
+	  	},
+	}
+);
 
 let render = () => {
     const MainApp = require('./MainApp').default;
     ReactDOM.render(
-        <MainApp/>,
+        (<MuiThemeProvider theme={theme}><MainApp/></MuiThemeProvider>),
         rootEl
     );
 };
