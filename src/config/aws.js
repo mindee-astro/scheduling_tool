8c4d1573fc35ad012a3f535f0a5151b02969ed0c
jinified <jinified@gmail.com>
2018-12-10 00:56:12 +0800

    Deployed cognito user pool and added development guide for authentication

--- src/config/aws.js
+++ src/config/aws.js
@@ -1,7 +1,7 @@
 module.exports = {
     cognito: {
         REGION: 'ap-southeast-1',
-        USER_POOL_ID: 'YOUR_COGNITO_USER_POOL_ID',
-        APP_CLIENT_ID: 'YOUR_COGNITO_APP_CLIENT_ID',
+        USER_POOL_ID: 'ap-southeast-1_s3NQJlkqf',
+        APP_CLIENT_ID: 'astro-tap-portal.s3-website-ap-southeast-1.amazonaws.com'
     }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,10 @@ output "portal_url" {
     value = "${aws_s3_bucket.portal_bucket.website_endpoint}"
 }
 
-output "cognito_userpool" {
-    value = ["${aws.aws_cognito_user_pool.pool.*}"]
+output "cognito_userpool_id" {
+    value = "${aws_cognito_user_pool.pool.id}"
 }
 
-output "cognito_client" {
-    value = "${aws.aws_cognito_user_pool_client.client.id}"
+output "cognito_client_id" {
+    value = "${aws_cognito_user_pool_client.client.id}"
 }
