c628f8d40b6ffe1800b6f4da803f74e379c73948
Yap Min Dee <min-dee_yap@astro.com.my>
2019-02-14 14:28:57 +0800

    protegeCard - connect api first commit

--- src/app/route/protege/protegeCard.js
+++ src/app/route/protege/protegeCard.js
@@ -45,22 +45,23 @@ const styles = theme => ({
 	}
 });
 
-let id = 0;
-function createData(username, display_name, join_date, next_rotation, next_startdate) {
-	id += 1;
-	return { id, username, display_name, join_date, next_rotation, next_startdate };
-}
+// let id = 0;
+// function createData(username, display_name, join_date, next_rotation, next_startdate) {
+// 	id += 1;
+// 	return { id, username, display_name, join_date, next_rotation, next_startdate };
+// }
 
-const rows = [
-	createData('MDYMINDE', 'Min Dee Yap', '2017-05-01', 'Software Engineering', '2018-11-13'),
-	createData('SJTSOONJ', 'Soon Jin Tan', '2017-08-08', 'Software Engineering', '2018-11-20'),
-	createData('JTTHAVAK', 'Thava', '2017-08-15', 'Products Engineering', '2018-12-25'),
-	createData('AAAAHMAD', 'Akmaluddin', '2017-09-04', 'Analytics', '2018-11-29'),
-	createData('CPSPEISH', 'Brianna Chang', '2017-09-04', 'Security', '2019-01-01'),
-	createData('OAARAKMA', 'Akmal', '2017-12-01', 'Product Management', '2019-01-01'),
-	createData('LWYWEIYE', 'Chris Lim', '2018-01-01', 'Product Management', '2019-01-01')
-];
 
+
+// const rows = [
+// 	createData('MDYMINDE', 'Min Dee Yap', '2017-05-01', 'Software Engineering', '2018-11-13'),
+// 	createData('SJTSOONJ', 'Soon Jin Tan', '2017-08-08', 'Software Engineering', '2018-11-20'),
+// 	createData('JTTHAVAK', 'Thava', '2017-08-15', 'Products Engineering', '2018-12-25'),
+// 	createData('AAAAHMAD', 'Akmaluddin', '2017-09-04', 'Analytics', '2018-11-29'),
+// 	createData('CPSPEISH', 'Brianna Chang', '2017-09-04', 'Security', '2019-01-01'),
+// 	createData('OAARAKMA', 'Akmal', '2017-12-01', 'Product Management', '2019-01-01'),
+// 	createData('LWYWEIYE', 'Chris Lim', '2018-01-01', 'Product Management', '2019-01-01')
+// ];
 
 
 class CustomizedTable extends Component {
@@ -91,16 +92,76 @@ class CustomizedTable extends Component {
 			this.setState({
 				...this.state,
 				allSchedule: this.props.allSchedule,
-				protegeNum: this.props.allSchedule.length
+				allScheduleLength: this.props.allSchedule.length
 			})
 			console.log('printing all schedule',this.state.allSchedule)
-			console.log('printing schedule pK',this.state.allSchedule.pK)
 		}
 	}
 
+	// TODO: figure out how to filter out schedule based on username
+	filteredUser(username){
+		// Try this
+		// Object.values(b.filter(user => Object.keys(user)[0] === 'a')[0])[0]
+		if(this.state.allScheduleLength>0){
+			let filteredUser = this.state.allSchedule.filter(s => Object.keys(s)[0] === username)[0]
+			let filteredUserValue = Object.values(filteredUser)[0]
+			console.log('filteredValue',filteredUserValue)
+			return filteredUserValue
+			// let filtered = Object.values(this.state.allSchedule.filter(user => Object.keys(user)[0]==='hhkahmad')[0])[0]
+			// console.log('filtered',filtered)
+			// let test = Object.values(this.state.allSchedule.filter(s => Object.keys(s)[0] === 'CPSHEISH')[0])[0]
+			// let test = Object.values(this.state.allSchedule.filter(s => {
+			// 	console.log(s);
+			// 	console.log(Object.keys(s));
+			// 	return Object.keys(s)[0] === 'CPSHEISH';
+			// })[0])[0];
+			// console.log(test);
+
+		}
+	}
+
+	findCurrentRotation(username) {
+		let currentDate = new Date().toISOString().split('T')[0]
+		console.log('currentdate',currentDate)
+
+		// let filteredUserValue = [{endDate: '2019-04-01',rotationID: 'PMO', startDate: '2019-02-01'}, {endDate: '2019-06-01',rotationID: 'SE', startDate: '2019-04-01'}]
+		let filteredUserValue = this.filteredUser(username)
+
+		// Find current rotation object
+		let filteredStartDate = filteredUserValue.filter(f => f.startDate <= currentDate)[0]['startDate']
+		let filteredEndDate = filteredUserValue.filter(f => f.endDate >= currentDate)[0]['endDate']
+
+		let test = filteredUserValue.findIndex(index => index.endDate ===filteredEndDate)
+		let nextRotationID = filteredUserValue[test+1].rotationID
+		let nextRotationStartDate = filteredUserValue[test+1].startDate
+		// let filteredStartDateValue = Ob
+		console.log('filteredStartDate',filteredStartDate)
+		console.log('filteredEndDate',filteredEndDate)
+		console.log('test',test)
+		console.log('nextRotationID',nextRotationID)
+		console.log('nextRotationStartDate',nextRotationStartDate)
+
+		return [nextRotationID,nextRotationStartDate]
+	}
+		// console.log('username',username)
+		// let usernameUpperCase = 'hhkahmad'.toUpperCase()
+		// let filteredKeys = Object.keys(this.state.allSchedule)
+		// let filteredValues = Object.values(this.state.allSchedule).filter.includes(usernameUpperCase)
+		// let filtered = Object.keys(this.state.allSchedule)
+		// .filter(key =>usernameUpperCase.includes(key))
+		// .reduce((obj,key)=>{
+		// 	return{
+		// 		...obj,
+		// 		[key]: this.state.allSchedule[key]
+		// 	};
+		// },{});
+	// }
+
+
+
 	render(){
 		const classes = this.state.classes;
-		const renderInfo = (this.state.listUserLength>0 && this.state.protegeNum>0) ? (
+		const renderInfo = (this.state.listUserLength>0 && this.state.allScheduleLength>0) ? (
 			<Paper className={classes.overflowXTable}>
 				<Table>
 					<TableHead>
@@ -118,13 +179,15 @@ class CustomizedTable extends Component {
 
 							// Time
 							let current_date = Date.now()
-							let next_startdate = new Date(row.next_startdate).getTime()
+							let next_startdate = new Date(this.findCurrentRotation(row.pK)[1]).getTime()
 							let days_to_start = Math.ceil((next_startdate - current_date) / (1000 * 60 * 60 * 24))
 							let starting_in = days_to_start.toString() + ' day' + (days_to_start > 1 ? 's' : '')
 							if (days_to_start >= 30) {
 								let month = Math.round(days_to_start / 30 * 10) / 10
 								starting_in = month.toFixed(1) + ' month' + (month > 1 ? 's' : '')
 							}
+						
+							
 							
 							return (
 								<TableRow key={index}>
@@ -132,9 +195,8 @@ class CustomizedTable extends Component {
 									<TableCell component="th" scope="row">{row.pK}</TableCell>
 									<TableCell>{row.displayName}</TableCell>
 									<TableCell>{row.joinDate}</TableCell>
-									{/* TODO: rearrange schedule array to match row.pK */}
-									<TableCell>{row.next_rotation}</TableCell>
-									<TableCell className={days_to_start > 10 ? '' : classes.redText}>{row.next_startdate}</TableCell>
+									<TableCell>{this.findCurrentRotation(row.pK)[0]}</TableCell>
+									<TableCell className={days_to_start > 10 ? '' : classes.redText}>{this.findCurrentRotation(row.pK)[1]}</TableCell>
 									<TableCell className={days_to_start > 10 ? '' : classes.redText}>{starting_in}</TableCell>
 								</TableRow>
 							);
